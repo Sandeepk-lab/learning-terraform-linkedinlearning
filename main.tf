94b227a724df286bc56726f023e320e214e795ce
Sandeepk-lab <dbasandeep9@gmail.com>
2024-10-05 08:28:11 -0400

    changed details in instance

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami2" {
 }
 
 resource "aws_instance" "blog2" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = data.aws_ami.app_ami2.id
   instance_type = var.instance_type
   vpc_security_group_ids= [aws_security_group.first_security_group.id]
 
