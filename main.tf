9812c973ea84c5a7d81b468d28dcb6bd971e50fd
Sandeepk-lab <dbasandeep9@gmail.com>
2024-10-30 00:01:11 -0400

    create ec2 instance 9th try , with out vpc_security_group_ids

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "blog2" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids= [aws_security_group.first_security_group.id]
+  #vpc_security_group_ids= [aws_security_group.first_security_group.id]
 
   tags = {
     Name = "EC2INSTANCE2"
