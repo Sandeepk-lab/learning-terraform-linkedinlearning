ecb9f270c90af46b430b6fa5493d9887bc0e0a9e
Sandeepk-lab <dbasandeep9@gmail.com>
2024-11-18 22:30:35 -0500

    re-creating security group with correct ingress rules

--- main.tf
+++ main.tf
@@ -72,7 +72,8 @@ module "sg" {
   description = "Security group within VPC"
   vpc_id      = data.aws_vpc.First_Default_VPC.id
 
-  ingress_rules            = ["https-443-tcp","http-80-tcp","http-8080-tcp","https-8443-tcp"]
+  #ingress_rules            = ["https-443-tcp","http-80-tcp","http-8080-tcp","https-8443-tcp"]
+  ingress_rules            = ["http-80-tcp","http-8080-tcp"]
    egress_rules            = ["all-all"]
 }
 
